785490571ad62f86309946f309422953ef4e2aae
lawmaso <masonlaw.cs@gmail.com>
2026-08-01 22:54:11 -0400

    solve(optimal): largest rectangle in histogram (.py)

--- stack/0084_largest_rectangle_in_histogram.py
+++ stack/0084_largest_rectangle_in_histogram.py
@@ -12,7 +12,7 @@ The largest rectangle is shown in the red area, which has an area = 10 units.
 """
 
 class Solution:
-    def largestRectangleArea(self, heights: List[int]) -> int:
+    def brute_largestRectangleArea(self, heights: list[int]) -> int:
         n = len(heights)
         res = 0
 
@@ -32,8 +32,72 @@ class Solution:
 
         return res
 
+    def largestRectangleArea(self, heights: list[int]) -> int:
+        n = len(heights)
+        res = 0
+
+        stack = []  # (index, height)
+        for i, h in enumerate(heights):
+            leftmost = i
+
+            # previous height is larger (expansion ends)
+            while stack and stack[-1][-1] > h:
+                prev_i, prev_h = stack.pop()
+                leftmost = prev_i
+
+                # area of previous histogram being expanded just before i
+                res = max(res, prev_h * ((i - 1) - prev_i + 1))
+
+            stack.append((leftmost, h))
+
+        # account for fully expanded histograms
+        while stack:
+            i, h = stack.pop()
+            res = max(res, h * ((n-1) - i + 1))
+
+        return res
+
+"""
+dry-run of v2 (optimal):
+
+   X
+  XX
+  XX
+  XX X
+X XXXX
+XXXXXX
+
+stack = [
+    (0,2),  a=2
+    (0,1),
+    (2,5),  a=10
+    (3,6),  a=6
+    (2,2),
+    (5,3)
+]
+
+    2 > 1:
+    0, 2 = st.pop()
+    a = 2 * (0-0+1) = 2
+    
+    6 > 2:
+    3, 6 = st.pop()
+    a = 6 * (3-3+1) = 6
+
+    5 > 2:
+    2, 5 = st.pop()
+    a = 5 * (3-2+1) = 10
+
+reverse order: (left to right)
+0,1 = st.pop(); a = 1 * (6) = 6
+2,2 = st.pop(); a = 2 * (5-2+1) = 8
+5,3 = st.pop(); a = 3 * (5-5+1) = 5
+
+passes
 """
 
+
+"""
 brute force: just try every possible histogram
 
 at each point, expand as far left and right to get max
@@ -59,5 +123,33 @@ at i=5: l=5, r=5, area = 3
 
 T: O(n^2)  [worst-case; ~n expansion cost per each of n columns]
 S: O(1)
+"""
+
+"""
+optimization: utilize stack to compute areas iteratively
+
+# (index, height)
+stack = [
+    (0, 2)+
+    (0, 1)-
+    (2, 5)+
+    (2, 5)+
+    (2, 2)-
+    (5, 3)-
+]
+
+-: not explicitly computed (fully expandable to the end)
++: computed throughout iterations
+
+compute iteratively (+) when a lower height is reached further down
+(i.e., it kind of cuts off the higher height from expanding further)
+
+res = arg_max (h * (r - l + 1))
+
+at end, stack will have the fully expandable histograms,
+so we'll need one last loop to compute areas
+
+T: O(n)  [going through all of heights]
+S: O(n)  [worst-case: strictly increasing heights]
 """
 
