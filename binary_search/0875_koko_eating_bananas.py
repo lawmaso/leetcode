31394c936f503cf7e6ded7201e04ea3726486968
lawmaso <masonlaw.cs@gmail.com>
2026-08-02 00:36:50 -0400

    solve(brute): koko eating bananas (.py)

---
+++ binary_search/0875_koko_eating_bananas.py
@@ -0,0 +1,71 @@
+"""
+Koko loves to eat bananas. There are n piles of bananas,
+the ith pile has piles[i] bananas. The guards have gone
+and will come back in h hours.
+
+Koko can decide her bananas-per-hour eating speed of k.
+Each hour, she chooses some pile of bananas and eats k
+bananas from that pile. If the pile has less than k bananas,
+she eats all of them instead and will not eat any more
+bananas during this hour.
+
+Koko likes to eat slowly but still wants to finish eating all
+the bananas before the guards return.
+
+Return the minimum integer k such that she can eat all
+the bananas within h hours.
+
+Example 1:
+Input: piles = [3,6,7,11], h = 8
+Output: 4
+
+Example 2:
+Input: piles = [30,11,23,4,20], h = 5
+Output: 30
+
+Example 3:
+Input: piles = [30,11,23,4,20], h = 6
+Output: 23
+"""
+
+import math
+
+class Solution:
+    def brute_minEatingSpeed(self, piles: list[int], h: int) -> int:
+        n = len(piles)
+        mx = res = max(piles)
+
+        for bph in range(1, mx + 1):
+            took = 0  # hours taken
+
+            for b in piles:
+                took += math.ceil(b / bph)  # b/bph = time
+                # ex: b=1, bph=3; b / bph = 0.33, but need int hours
+                # ceil(0.33) = 1
+
+            # eaten all piles within the time
+            if took <= h:
+                res = min(res, bph)
+                break
+
+                # NOTE: we can break here since we want the min
+                #       eating speed that allows koko to eat all piles
+                #       within h hours, which is just that
+
+        return res
+
+"""
+brute force: try all eating speeds
+
+lowest speed would be 1
+highest speed would be max(piles)
+
+for each speed, see how long it would take to eat
+all the bananas
+
+res = arg_min [...]
+
+T: O(max(piles) * n)
+S: O(1)
+"""
+
