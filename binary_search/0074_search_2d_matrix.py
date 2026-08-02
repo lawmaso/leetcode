829e4fe9b8b42f9fda396c0219749fd0e0932ddd
lawmaso <masonlaw.cs@gmail.com>
2026-08-02 00:09:11 -0400

    solve(optimal): search 2d matrix (.py)

--- binary_search/0074_search_2d_matrix.py
+++ binary_search/0074_search_2d_matrix.py
@@ -19,10 +19,50 @@ class Solution:
 
         return False
 
+    def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])
+
+        left, right = 0, m * n - 1
+        while left <= right:
+            mid = left + (right-left) // 2
+            
+            r = mid // n  # "number of full columns"
+            c = mid % n   # "column"
+
+            mid_val = matrix[r][c]
+            if mid_val == target:
+                return True
+            elif mid_val < target:  # too small, search rightward
+                left = mid + 1
+            else:  # too large, search leftward
+                right = mid - 1
+
+        return False
+
 """
 brute force: just linear search
 
 T: O(mn)
+S: O(1)
+"""
+
+"""
+optimal: flatten matrix and binary search on regular list
+
+matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+
+1 3 5 7 10 11 16 20 23 30 34 60
+l                            r
+
+need a way to get from (r, c) indexing to flattened
+
+for any [r][c], flat_index = (n * r) + c
+
+or to get from flat index to [r][c]:
+    r = flat_index // n  (full columns)
+    c = flat_index % n   (column)
+
+T: O(log(mn))
 S: O(1)
 """
 
