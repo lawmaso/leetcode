0e4dfcfe353a4c55a404f9fe1738041e8f1ae4f0
lawmaso <masonlaw.cs@gmail.com>
2026-08-02 23:41:25 -0400

    solve(bfs): invert binary tree (.py)

--- trees/0226_invert_binary_tree.py
+++ trees/0226_invert_binary_tree.py
@@ -11,7 +11,7 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def invertTree(self, root: TreeNode | None) -> TreeNode | None:
+    def recursive_invertTree(self, root: TreeNode | None) -> TreeNode | None:
         if not root:
             return None
 
@@ -22,6 +22,25 @@ class Solution:
         # invert at this level
         root.left, root.right = right, left
         
+        return root
+    
+    def iterative_invertTree(self, root: TreeNode | None) -> TreeNode | None:
+        from collections import deque
+
+        if not root:
+            return None
+
+        q = deque([root])
+        while q:
+            node = q.popleft()
+
+            # swap children
+            node.left, node.right = node.right, node.left
+
+            # continue on non-null children
+            if node.left:  q.append(node.left)
+            if node.right: q.append(node.right)
+
         return root
 
 """
@@ -36,5 +55,27 @@ at 2: root.left, root.right  root.right, root.left
 
 T: O(n)  [each node visited once]
 S: O(h)  [height of tree; between O(log(n)) and O(n)]
+"""
+
+"""
+iterative: bfs
+
+ex:
+[2,1,3]
+
+    2
+1       3
+
+queue = [(root, left, right)]
+head, l, r = queue.popleft()
+
+... [continue on children]
+
+on second thought, we don't need the queue to have left and right
+children as that's already a given inside of the node in each tuple
+entry
+
+T: O(n)  [visit each node]
+S: O(n)  [queue length at last level ~= n/2]
 """
 
