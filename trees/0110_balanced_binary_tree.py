84873daf80b646f703b721505b493adff46a4391
lawmaso <masonlaw.cs@gmail.com>
2026-08-04 15:14:05 -0400

    solve(clean): balanced binary tree (.py)

--- trees/0110_balanced_binary_tree.py
+++ trees/0110_balanced_binary_tree.py
@@ -15,7 +15,18 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def isBalanced(self, root: TreeNode | None) -> bool:
+    def old_isBalanced(self, root: TreeNode | None) -> bool:
+        """
+        want children heights to never diff by more than 1
+
+        want abs(lh - rh) <= 1
+
+        bottom-up augmented dfs that returns
+        tuple of (valid: bool, height: int)
+
+        T: O(n)
+        S: O(h)
+        """
         def dfs(node: TreeNode | None) -> tuple[bool, int]:
             if not node:
                 return True, 0
@@ -33,6 +44,34 @@ class Solution:
 
         return dfs(root)[0]
 
+    def isBalanced(self, root: TreeNode | None) -> bool:
+        """
+        to make the code less verbose, we can encode
+        validity into the height itself as -1 since heights start at 0
+
+        same time and space as the original solution
+        """
+
+        def dfs(node: TreeNode | None) -> int:
+            if not node:
+                return 0
+
+            left_height = dfs(node.left)
+            if left_height == -1:
+                return left_height
+
+            right_height = dfs(node.right)
+            if right_height == -1:
+                return right_height
+
+            diff = abs(right_height - left_height)
+            return 1 + max(left_height, right_height) if diff <= 1 else -1
+
+
+        # if tree is balanced; all checks will pass and we'll
+        # get returned a non-negative height
+        return dfs(root) != -1
+
 """
 ex1:
 
@@ -42,17 +81,5 @@ Output: true
         3
     9       20
           15  7
-"""
-
-"""
-want children heights to never diff by more than 1
-
-want abs(lh - rh) <= 1
-
-bottom-up augmented dfs that returns
-tuple of (valid: bool, height: int)
-
-T: O(n)
-S: O(h)
 """
 
