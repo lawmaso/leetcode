a5cdd50b656c9393a6dd0b2918fe9cf82e14915c
lawmaso <masonlaw.cs@gmail.com>
2026-08-05 14:20:59 -0400

    solve(optimal): validate binary search tree (.py)

--- trees/0098_validate_binary_search_tree.py
+++ trees/0098_validate_binary_search_tree.py
@@ -43,6 +43,39 @@ class Solution:
         dfs(root)
         return all(vals[i - 1] < vals[i] for i in range(1, len(vals)))
 
+    def isValidBST(self, root: TreeNode | None) -> bool:
+        """
+        recursive dfs with bounds 
+        
+        after each node is processed, the value becomes
+        the new boundary
+
+        if left, val is the new upper bound
+        if right, val is the new lower bound
+
+        to validate, we just need to check all values
+        are within the range (lo, hi)
+
+        T: O(n)
+        S: O(h)
+        """
+
+        def dfs(
+            node: TreeNode | None,
+            lo: float | int = float("-inf"),
+            hi: float | int = float("inf")
+        ) -> bool:
+            if not node:
+                return True
+
+            return (
+                lo < node.val < hi and
+                dfs(node.left, lo, node.val) and
+                dfs(node.right, node.val, hi)
+            )
+
+        return dfs(root)
+
 """
 ex1:
 
@@ -62,4 +95,23 @@ ex2:
 
 [1,5,3,4,6]- (pass)
    |-|
+"""
+
+"""
+ex1 [optimal]: [2,1,3]
+        2
+    1       3
+
+2 in [-inf, inf]+
+1 in [-inf, 2]+
+3 in [2, inf]+
+
+ex2 [optimal]: [5,1,4,null,null,3,6]
+            5
+        1       4
+              3   6
+
+5 in [-inf, inf]+
+1 in [-inf, 5]+
+4 in [5, inf]-  <-> invalidates entire tree
 """
