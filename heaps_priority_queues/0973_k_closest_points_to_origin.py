ce6a96de5f4d5c8082f098415b3b9a4efb0a5829
lawmaso <masonlaw.cs@gmail.com>
2026-08-06 18:54:45 -0400

    solve(optimal): k closest points to origin (.py)

--- heaps_priority_queues/0973_k_closest_points_to_origin.py
+++ heaps_priority_queues/0973_k_closest_points_to_origin.py
@@ -42,3 +42,76 @@ class Solution:
             [x, y]
             for _, x, y in point_tuples[:k]
         ]
+
+    def kClosest(self, points: list[list[int]], k: int) -> list[list[int]]:
+        """
+        New approach: full min heap
+
+        T: O(n + klog(n))
+        S: O(n)
+        """
+        import heapq
+
+        def sq_dist(x: int, y: int) -> int:
+            # sqrt[(x - 0)^2 + (y - 0)^2]
+            # == sqrt(x^2 + y^2) 
+
+            # sqrt is strictly increasing, so we can
+            # just return the square of the distance
+            # same result either way + lower computation cost
+            return x*x + y*y
+
+        # build out the min heap
+        heap = [
+            (sq_dist(x, y), x, y)
+            for x, y in points
+        ]
+        heapq.heapify(heap)
+
+        res = []
+
+        # pop off the k mins
+        for _ in range(k):
+            _, x, y = heapq.heappop(heap)
+            res.append([x, y])
+
+        return res
+
+    def kClosest(self, points: list[list[int]], k: int) -> list[list[int]]:
+        """
+        Optimal approach: heap of size k
+
+        Want to keep the minimum k elements
+
+        Max heap of size k: when the heap becomes too large,
+        popping will remove the maxes, which is what we need
+
+        T: O(nlog(k) + k) = O(nlog(k))
+        S: O(k)
+        """
+        import heapq
+
+        def sq_dist(x: int, y: int) -> int:
+            # sqrt[(x - 0)^2 + (y - 0)^2]
+            # == sqrt(x^2 + y^2) 
+
+            # sqrt is strictly increasing, so we can
+            # just return the square of the distance
+            # same result either way + lower computation cost
+            return x*x + y*y
+
+        heap = []
+
+        for x, y in points:
+            dist = sq_dist(x, y)
+            heapq.heappush(heap, (-dist, x, y))
+
+            # heap is overpopulated
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        # unnegate + get the k closest
+        return [
+            [x, y]
+            for _, x, y in heap
+        ]
