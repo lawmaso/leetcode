0837dddda3fae9bedad0d72315e32522f88a31f4
lawmaso <masonlaw.cs@gmail.com>
2026-08-06 19:34:02 -0400

    solve(optimal): kth largest element in an array (.py)

--- heaps_priority_queues/0215_kth_largest_element_in_an_array.py
+++ heaps_priority_queues/0215_kth_largest_element_in_an_array.py
@@ -18,3 +18,25 @@ class Solution:
         """
         nums.sort()
         return nums[len(nums) - k]  # (n-1) - (k+1)
+
+    def findKthLargest(self, nums: list[int], k: int) -> int:
+        """
+        Optimal approach: min heap of size k
+
+        The kth largest will become the root of the heap
+
+        T: O(nlog(k))
+        S: O(n)
+        """
+        import heapq
+        heap = []
+
+        for n in nums:
+            # push value to heap
+            heapq.heappush(heap, n)
+
+            # stabilize heap length back to k
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return heap[0]
